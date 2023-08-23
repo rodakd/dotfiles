ab13205e3be5ec50826a8602eab88a4e38cdec82
Dawid Rodak <dawidrodak@yahoo.com>
2023-08-24 00:51:05 +0200

    Simplify neovim config

--- init.lua
+++ init.lua
@@ -1,233 +1,172 @@
--- Plugin
 require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("folke/tokyonight.nvim")
-	use("tpope/vim-commentary")
-	use("neovim/nvim-lspconfig")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("saadparwaiz1/cmp_luasnip")
-	use("L3MON4D3/LuaSnip")
-	use("nvim-lua/plenary.nvim")
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
-	use({ "nvim-telescope/telescope.nvim" })
-	use({
-		"ahmedkhalf/project.nvim",
-		config = function()
-			require("project_nvim").setup({})
-		end,
-	})
-	use("tpope/vim-fugitive")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("jay-babu/mason-null-ls.nvim")
-	use("windwp/nvim-ts-autotag")
-	use("tpope/vim-repeat")
-	use("nvim-pack/nvim-spectre")
-	use("nvim-tree/nvim-web-devicons")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-	use("windwp/nvim-autopairs")
-	use("tpope/vim-surround")
-	use("mbbill/undotree")
-	use("maxmellon/vim-jsx-pretty")
-	use("nvim-treesitter/nvim-treesitter-context")
-	use("tpope/vim-vinegar")
+    use("wbthomason/packer.nvim")
+    use("folke/tokyonight.nvim")
+    use("neovim/nvim-lspconfig")
+    use("nvim-treesitter/nvim-treesitter")
+    use("nvim-treesitter/nvim-treesitter-context")
+    use("nvim-lua/plenary.nvim")
+    use("nvim-telescope/telescope.nvim")
+    use("tpope/vim-commentary")
+    use("tpope/vim-vinegar")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("hrsh7th/nvim-cmp")
+    use("windwp/nvim-ts-autotag")
+    use("windwp/nvim-autopairs")
+    use("mbbill/undotree")
+    use("maxmellon/vim-jsx-pretty")
+    use("nvim-tree/nvim-web-devicons")
 end)
 
-require("nvim-autopairs").setup({
-	disable_filetype = { "TelescopePrompt", "vim" },
-})
-require("treesitter-context").setup()
-require("nvim-web-devicons").setup()
-require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-	},
-	autotag = {
-		enable = true,
-	},
-	context_commentstring = {
-		enable = true,
-	},
-})
-
-local luasnip = require("luasnip")
 local cmp = require("cmp")
+local treesitter_context = require("treesitter-context")
+local treesitter_configs = require("nvim-treesitter.configs")
+local nvim_autopairs = require("nvim-autopairs")
+local telescope = require("telescope")
+local telescope_actions = require("telescope.actions")
+local telescope_builtin = require("telescope.builtin")
+local lspconfig = require("lspconfig")
+local nvim_web_devicons = require("nvim-web-devicons")
+
+treesitter_context.setup()
+nvim_web_devicons.setup()
+nvim_autopairs.setup({ disable_filetype = { "TelescopePrompt", "vim" } })
+lspconfig.clangd.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.gopls.setup {}
+
+lspconfig.lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' },
+            },
+        },
+    },
+}
+
+local prettierFormat = { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+local styluaFormat = { formatCommand = "stylua --stdin-filepath ${INPUT}", formatStdin = true }
+
+lspconfig.efm.setup {
+    init_options = { documentFormatting = true },
+    root_dir = lspconfig.util.root_pattern { ".git/", "." },
+    filetypes = {
+        "html",
+        "css",
+        "scss",
+        "json",
+        "yaml",
+        "lua",
+        "markdown",
+        "javascript",
+        "typescript",
+        "javascriptreact",
+        "typescriptreact",
+    },
+    settings = {
+        rootMarkers = { ".git/" },
+        languages = {
+            html = { prettierFormat },
+            css = { prettierFormat },
+            scss = { prettierFormat },
+            json = { prettierFormat },
+            yaml = { prettierFormat },
+            markdown = { prettierFormat },
+            javascript = { prettierFormat },
+            typescript = { prettierFormat },
+            javascriptreact = { prettierFormat },
+            typescriptreact = { prettierFormat },
+            lua = { styluaFormat },
+        }
+    }
+}
+
+treesitter_configs.setup({
+    ensure_installed = { "c", "lua", "typescript", "javascript", "python", "go", "bash", "lua" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+    },
+    autotag = {
+        enable = true,
+    },
+    context_commentstring = {
+        enable = true,
+    },
+})
 
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-	},
+    mapping = cmp.mapping.preset.insert({
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    sources = {
+        { name = "nvim_lsp" },
+    },
 })
 
 cmp.setup.cmdline({ "/", "?" }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = "buffer" },
-	},
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" },
+    },
 })
 
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        { name = "cmdline" },
+    }),
 })
 
-local actions = require("telescope.actions")
-local telescopeb = require("telescope.builtin")
-require("telescope").setup({
-	defaults = {
-		layout_strategy = "horizontal",
-		layout_config = {
-			height = 0.95,
-			width = 0.95,
-			preview_width = 0.5,
-		},
-		mappings = {
-			i = {
-				["<esc>"] = actions.close,
-			},
-		},
-	},
+telescope.setup({
+    defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+            height = 0.99,
+            preview_cutoff = 10,
+            prompt_position = "bottom",
+            width = 0.99,
+        },
+        mappings = {
+            i = {
+                ["<esc>"] = telescope_actions.close,
+            },
+        },
+    },
 })
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-local lspconfig = require("lspconfig")
-require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		lspconfig[server_name].setup({
-			capabilities = capabilities,
-		})
-	end,
-	["lua_ls"] = function()
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-	end,
-})
-require("mason-null-ls").setup({
-	automatic_installation = false,
-	handlers = {},
-})
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local null_ls = require("null-ls")
-local lSsources = {
-	null_ls.builtins.formatting.prettierd.with({
-		filetypes = {
-			"typescript",
-			"typescriptreact",
-			"javascript",
-			"css",
-			"scss",
-			"html",
-			"json",
-			"yaml",
-			"markdown",
-			"md",
-			"txt",
-			"astro",
-		},
-	}),
-}
-
-null_ls.setup({
-	sources = lSsources,
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						bufnr = bufnr,
-						filter = function(c)
-							return c.name == "null-ls"
-						end,
-						async = false,
-					})
-				end,
-			})
-		end
-	end,
-})
-
-vim.cmd([[
-if has("persistent_undo")
-   let target_path = expand('~/.undodir')
-
-    " create the directory and any parent directories
-    " if the location does not exist.
-    if !isdirectory(target_path)
-        call mkdir(target_path, "p", 0700)
-    endif
-
-    let &undodir=target_path
-    set undofile
-endif
-]])
-
--- Defaults
-vim.opt.signcolumn = "no"
+vim.g.mapleader = " "
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
+vim.g.netrw_keepdir = 0
 vim.o.ruler = false
 vim.o.laststatus = 0
-vim.g.mapleader = " "
+vim.opt.signcolumn = "no"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.hlsearch = false
@@ -235,33 +174,8 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = false
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
-vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gr", function()
-			telescopeb.lsp_references({ trim_text = true, show_line = false })
-		end, opts)
-		vim.keymap.set("n", "gd", telescopeb.lsp_definitions, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set({ "n", "v" }, "L", function()
-			vim.diagnostic.open_float(0, { scope = "line" })
-		end, opts)
-		vim.keymap.set("n", "<C-b>", function()
-			telescopeb.diagnostics()
-		end, {})
-		vim.keymap.set("n", "<C-m>", function()
-			vim.diagnostic.goto_next({ float = false })
-		end, {})
-	end,
-})
-
--- Indentation
+vim.opt.isfname:append("@-@")
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -269,11 +183,8 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
--- Netrw
-vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
-vim.g.netrw_keepdir = 0
-
--- Colorscheme
+vim.cmd("autocmd BufRead,BufNewFile Jenkinsfile set filetype=groovy")
+vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
 vim.cmd("colorscheme tokyonight-night")
 vim.cmd("hi NormalNC ctermbg=NONE guibg=NONE")
 vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
@@ -283,131 +194,86 @@ vim.cmd("hi LineNr guifg='#888888'")
 vim.cmd("hi CursorLineNr guifg='#87afff'")
 vim.cmd("hi TelescopeNormal cterm=NONE guibg=NONE")
 vim.cmd("hi TelescopeBorder cterm=NONE guibg=NONE guifg='#87afff'")
-
--- Tmux
 vim.cmd('autocmd BufEnter * call system("tmux rename-window " . expand("%"))')
 vim.cmd('autocmd VimLeave * call system("tmux rename-window zsh")')
+vim.cmd("nnoremap <C-i> :b# <CR>")
+vim.cmd("nnoremap <C-l> <C-o>")
 
--- Keymaps
-vim.keymap.set("n", "<C-s>", telescopeb.git_status, {})
-vim.keymap.set("n", "<C-p>", telescopeb.git_files, {})
-vim.keymap.set("n", "<C-o>", telescopeb.oldfiles, {})
-vim.keymap.set("n", "<C-f>", telescopeb.live_grep, {})
-vim.keymap.set("n", "<C-h>", telescopeb.help_tags, {})
-vim.keymap.set("n", "<C-y>", telescopeb.resume, {})
-vim.keymap.set("n", "<leader>Y", function()
-	vim.cmd(':let @+ = expand("%")')
-end, {})
-vim.keymap.set("n", "<C-g>", function()
-	vim.cmd("G")
-end)
-vim.keymap.set("n", "<C-t>", function()
-	vim.cmd("UndotreeToggle")
-	vim.cmd("UndotreeFocus")
-end)
+vim.cmd([[
+if has("persistent_undo")
+    let target_path = expand('~/.undodir')
+
+    if !isdirectory(target_path)
+        call mkdir(target_path, "p", 0700)
+    endif
+
+    let &undodir=target_path
+    set undofile
+endif
+]])
+
+vim.keymap.set("n", "<C-s>", telescope_builtin.git_status, {})
+vim.keymap.set("n", "<C-p>", telescope_builtin.git_files, {})
+vim.keymap.set("n", "<C-o>", telescope_builtin.oldfiles, {})
+vim.keymap.set("n", "<C-f>", telescope_builtin.live_grep, {})
+vim.keymap.set("n", "<C-h>", telescope_builtin.help_tags, {})
+vim.keymap.set("n", "<C-y>", telescope_builtin.resume, {})
 vim.keymap.set("n", "<leader>w", vim.cmd.w, {})
 vim.keymap.set("n", "<leader>p", '"+p')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.cmd("nnoremap <C-i> :b# <CR>")
-vim.cmd("nnoremap <C-l> <C-o>")
 
--- Filter diagnostics
-local ignore = {
-	["Pyright"] = {
-		'^".+" is unknown import symbol$',
-	},
-}
+vim.keymap.set("n", "<leader>Y", function()
+    vim.cmd(':let @+ = expand("%")')
+end, {})
 
-function Filter(arr, func)
-	local new_index = 1
-	local size_orig = #arr
-	for old_index, v in ipairs(arr) do
-		if func(v, old_index) then
-			arr[new_index] = v
-			new_index = new_index + 1
-		end
-	end
-	for i = new_index, size_orig do
-		arr[i] = nil
-	end
-end
+vim.keymap.set("n", "<C-g>", function()
+    vim.cmd("G")
+end)
 
-function Filter_diagnostics(diagnostic)
-	for source, messages in pairs(ignore) do
-		if source == diagnostic.source then
-			for i, message in ipairs(messages) do
-				if string.match(diagnostic.message, message) then
-					return false
-				end
-			end
-		end
-	end
-	return true
-end
+vim.keymap.set("n", "<C-t>", function()
+    vim.cmd("UndotreeToggle")
+    vim.cmd("UndotreeFocus")
+end)
 
-function Custom_on_publish_diagnostics(a, params, client_id, c, config)
-	Filter(params.diagnostics, Filter_diagnostics)
-	vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
-end
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        local opts = { buffer = ev.buf }
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = ev.buf })
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(Custom_on_publish_diagnostics, {
-	update_in_insert = true,
-	underline = false,
-})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = ev.buf,
+            callback = function()
+                vim.lsp.buf.format()
+            end,
+        })
 
--- Snippets
--- You can check file type with
--- print(vim.inspect(require("luasnip").get_snippet_filetypes()))`)
-local ls = require("luasnip")
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
+        vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-local function copy(args)
-	return args[1]
-end
+        vim.keymap.set("n", "gr", function()
+            telescope_builtin.lsp_references({ trim_text = true, show_line = false })
+        end, opts)
 
-ls.add_snippets("all", {}, { key = "all" })
+        vim.keymap.set({ "n", "v" }, "L", function()
+            vim.diagnostic.open_float(0, { scope = "line" })
+        end, opts)
 
-ls.add_snippets("python", {
-	s("def", {
-		t("def "),
-		i(1, "name"),
-		t("("),
-		i(2, "args"),
-		t({ "):", "\t" }),
-		i(3, "pass"),
-	}),
-}, {
-	key = "python",
-})
+        vim.keymap.set("n", "<C-b>", function()
+            telescope_builtin.diagnostics({
+                severity = "error",
+            })
+        end, {})
 
-ls.add_snippets("typescript", {
-	s("cl", {
-		t({ "console.log(`", "\t", "" }),
-		i(1),
-		t({ "", "", "`)" }),
-	}),
-}, {
-	key = "typescript",
-})
-
-ls.filetype_extend("typescriptreact", { "typescript" })
-ls.add_snippets("typescriptreact", {
-	s("rfc", {
-		t("type "),
-		f(copy, 1),
-		t({ "Props = {}", "", "export function " }),
-		i(1, "Component"),
-		t("({}: "),
-		f(copy, 1),
-		t({ "Props) {", "\t" }),
-		i(2, "return"),
-		t({ "", "}", "" }),
-	}),
-}, {
-	key = "typescriptreact",
+        vim.keymap.set("n", "<C-m>", function()
+            vim.diagnostic.goto_next({ float = false })
+        end, {})
+    end,
 })
