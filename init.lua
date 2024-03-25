841913b9be0974e8c4d4e7055627f454aeaf5dcb
Dawid Rodak <dawidrodak@yahoo.com>
2024-03-25 13:52:09 +0100

    Add import organization in go

--- init.lua
+++ init.lua
@@ -363,6 +363,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
                 filter = function(client) return client.name ~= "typescript-tools" end
             }
 
+            local bufName = vim.api.nvim_buf_get_name(ev.buf)
+            local isGo = bufName:sub(- #".go") == ".go"
+
+            if isGo then
+                local params = vim.lsp.util.make_range_params()
+                params.context = { only = { "source.organizeImports" } }
+                local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                for cid, res in pairs(result or {}) do
+                    for _, r in pairs(res.result or {}) do
+                        if r.edit then
+                            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                        end
+                    end
+                end
+            end
+
             vim.cmd.wall()
             vim.diagnostic.enable(ev.buf)
         end, opts)
