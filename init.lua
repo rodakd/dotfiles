8dffc8a0fc488197bb7ca91726a7217f0e508e3c
Dawid Rodak <dawidrodak@yahoo.com>
2024-05-14 20:11:37 +0200

    Fixes

--- init.lua
+++ init.lua
@@ -305,6 +305,7 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = false
+vim.opt.guicursor = ""
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
