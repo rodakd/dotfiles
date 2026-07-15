b7b34304dcfc33b7573eaae62544a218c80b89ff
Dawid Rodak <dawidrodak@yahoo.com>
2026-07-15 16:27:20 +0200

    Fix

--- init.lua
+++ init.lua
@@ -387,24 +387,7 @@ vim.lsp.config("clangd", {
 })
 
 vim.lsp.enable("clangd")
-
-vim.lsp.config.vtsls = {
-	settings = {
-		typescript = {
-			tsserver = {
-				maxTsServerMemory = 6144,
-			},
-		},
-		vtsls = {
-			autoUseWorkspaceTsdk = true,
-			typescript = {
-				globalTsdk = "",
-			},
-		},
-	},
-}
-
-vim.lsp.enable("vtsls")
+vim.lsp.enable("tsgo")
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
